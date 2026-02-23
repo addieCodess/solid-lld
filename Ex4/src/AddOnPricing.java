32b4016f34ef3d7e0aecc295ab34792a942cc88d
addieCodess <rimi.pits@gmail.com>
2026-02-23 11:52:09 +0530

    Create AddOnPricing.java

---
+++ Ex4/src/AddOnPricing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AddOnPricing implements PricingComponent {
+
+    public Money monthly(BookingRequest req) {
+
+        double add = 0.0;
+
+        for (AddOn a : req.addOns) {
+            if (a == AddOn.MESS) add += 1000.0;
+            else if (a == AddOn.LAUNDRY) add += 500.0;
+            else if (a == AddOn.GYM) add += 300.0;
+        }
+
+        return new Money(add);
+    }
+}
