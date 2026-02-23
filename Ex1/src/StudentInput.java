2f0a286a34b913924051e0229f3ee4cc91339a71
addieCodess <rimi.pits@gmail.com>
2026-02-23 11:25:00 +0530

    Add StudentInput class for student data management

---
+++ Ex1/src/StudentInput.java
@@ -0,0 +1,13 @@
+class StudentInput {
+    String name;
+    String email;
+    String phone;
+    String program;
+
+    public StudentInput(String name, String email, String phone, String program) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.program = program;
+    }
+}
